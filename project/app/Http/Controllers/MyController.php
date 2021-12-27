44b057c3386308846a4e415ef5267c1bfe05c38b
Y00nji <minsegye@gmail.com>
2021-12-27 12:10:30 +0300

    main

--- project/app/Http/Controllers/MyController.php
+++ project/app/Http/Controllers/MyController.php
@@ -6,10 +6,17 @@ use Illuminate\Http\Request;
 
 class MyController extends Controller
 {
-    public function enter() {
-        return 'авторизация';
+    public function login() {
+        return 'Ввод email';
+        return 'Ввод пароль';
     }
-    public function hello() {
-        return 'welcome';
+    public function registration() {
+        return 'Ввод email';
+        return 'Ввод ФИО';
+        return 'Ввод серия номер';
+        return 'Вывод пароль';
+    }
+    public function page() {
+        return 'вся информация о пользователе';
     }
 }
--- project/routes/web.php
+++ project/routes/web.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'MyController@hello');
+Route::get('Login', 'MyController@login');
 
-Route::get('page', 'MyController@enter');
+Route::get('Registration', 'MyController@registration');
+
+Route::get('MyPage', 'MyController@page');
